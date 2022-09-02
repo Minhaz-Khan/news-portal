53e91490cc27e1ab2d38337d7d7e07d476d07a7e
Minhaz-Khan <kminhaz111@gmail.com>
2022-09-03 00:27:58 +0600

    header section desgin done

---
+++ js/news.js
@@ -0,0 +1,17 @@
+// news item list section 
+const newsListContainer = document.getElementById('news-category');
+newsListContainer.innerHTML = `
+       <div class="row row-col-4 p-4 justify-content-around">
+            <div class="col text-center">Home</div>
+            <div class="col text-center">Breaking news</div>
+            <div class="col text-center">Regular news</div>
+            <div class="col-2 text-center">International news</div>
+            <div class="col text-center">Sports</div>
+            <div class="col text-center">Entertainment</div>
+            <div class="col text-center">Culture</div>
+            <div class="col text-center">Arts</div>
+            <div class="col text-center">All News</div>
+       </div>
+`
+
+
