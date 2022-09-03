0ff5e5e2c42b26339cdb677cbdcd8bb7b4e429fe
Minhaz-Khan <kminhaz111@gmail.com>
2022-09-04 01:20:53 +0600

    4 quation done

---
+++ js/blog.js
@@ -0,0 +1,6 @@
+document.getElementById('NewsButton').addEventListener('click', function () {
+    window.location.href = "index.html"
+})
+document.getElementById('blogButton').addEventListener('click', function () {
+    window.location.href = "blog.html"
+})
--- js/news.js
+++ js/news.js
@@ -1,20 +1,26 @@
 // news list section 
 const getAllNewsListData = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/news/categories');
-    const allNewsList = await res.json();
-    allListDisplay(allNewsList.data.news_category);
+    try {
+        const res = await fetch('https://openapi.programming-hero.com/api/news/categories');
+        const allNewsList = await res.json();
+        allListDisplay(allNewsList.data.news_category);
+    }
+    catch (error) {
+        console.log(error);
+    }
 }
 const allListDisplay = (allNewsList) => {
     const newsListContainer = document.getElementById('news-category');
     newsListContainer.innerHTML = `
-       <div class="row  p-4 justify-content-around fw-semibold">
-            <a class=" col-md col-4  text-center pe-auto cursor-set" onclick="getAllNewsData(${allNewsList[0].category_id},'${allNewsList[0].category_name}')">${allNewsList[0].category_name}</a>
-            <a class=" col-md col-4 text-center pe-auto cursor-set" onclick="getAllNewsData(${allNewsList[1].category_id},'${allNewsList[1].category_name}')">${allNewsList[1].category_name}</a>
-            <a class=" col-md col-4 text-center pe-auto cursor-set" onclick="getAllNewsData(${allNewsList[2].category_id},'${allNewsList[3].category_name}')">${allNewsList[3].category_name}</a>
+       <div class="row g-md-0 g- p-4 justify-content-around align-item-center fw-semibold">
+            <a class=" col-md col-3  text-center pe-auto cursor-set" onclick="getAllNewsData(${allNewsList[0].category_id},'${allNewsList[0].category_name}')">${allNewsList[0].category_name}</a>
+            <a class=" col-md col-3 text-center pe-auto cursor-set" onclick="getAllNewsData(${allNewsList[1].category_id},'${allNewsList[1].category_name}')">${allNewsList[1].category_name}</a>
+            <a class=" col-md col-4 text-center pe-auto cursor-set" onclick="getAllNewsData(${allNewsList[2].category_id},'${allNewsList[2].category_name}')">${allNewsList[2].category_name}</a>
+            <a class=" col-md col-3 text-center pe-auto cursor-set" onclick="getAllNewsData(${allNewsList[3].category_id},'${allNewsList[3].category_name}')">${allNewsList[3].category_name}</a>
             <a class=" col-md col-4 text-center pe-auto cursor-set" onclick="getAllNewsData(${allNewsList[4].category_id},'${allNewsList[4].category_name}')">${allNewsList[4].category_name}</a>
-            <a class=" col-md col-4 text-center pe-auto cursor-set" onclick="getAllNewsData(${allNewsList[5].category_id},'${allNewsList[5].category_name}')">${allNewsList[5].category_name}</a>
-            <a class=" col-md col-4 text-center pe-auto cursor-set" onclick="getAllNewsData(${allNewsList[6].category_id},'${allNewsList[6].category_name}')">${allNewsList[6].category_name}</a>
-            <a class=" col-md col-4 text-center pe-auto cursor-set" onclick="getAllNewsData(${allNewsList[7].category_id},'${allNewsList[7].category_name}')">${allNewsList[7].category_name}</a>
+            <a class=" col-md col-3 text-center pe-auto cursor-set" onclick="getAllNewsData(${allNewsList[5].category_id},'${allNewsList[5].category_name}')">${allNewsList[5].category_name}</a>
+            <a class=" col-md col-3 text-center pe-auto cursor-set" onclick="getAllNewsData(${allNewsList[6].category_id},'${allNewsList[6].category_name}')">${allNewsList[6].category_name}</a>
+            <a class=" col-md col-3 text-center pe-auto cursor-set" onclick="getAllNewsData(${allNewsList[7].category_id},'${allNewsList[7].category_name}')">${allNewsList[7].category_name}</a>
        </div>
     `
 }
@@ -22,11 +28,15 @@ const allListDisplay = (allNewsList) => {
 
 // display category news click by news list 
 const getAllNewsData = async (newsId, newsName) => {
-    console.log(newsName, newsId)
     isLoading(true);
-    const res = await fetch(`https://openapi.programming-hero.com/api/news/category/0${newsId}`);
-    const allNews = await res.json();
-    displayAllNews(allNews.data);
+    try {
+        const res = await fetch(`https://openapi.programming-hero.com/api/news/category/0${newsId}`);
+        const allNews = await res.json();
+        displayAllNews(allNews.data);
+    }
+    catch (error) {
+        console.log(error)
+    }
     // item found section 
     const numberofNews = allNews.data.length;
     const newsCount = document.getElementById('news-count');
@@ -57,8 +67,6 @@ const displayAllNews = (allNews) => {
 
 
     allNews.forEach(news => {
-        // console.log(news);
-
         const newsDiv = document.createElement('div');
         newsDiv.classList.add('card', 'mb-3');
         newsDiv.innerHTML = `
@@ -68,8 +76,8 @@ const displayAllNews = (allNews) => {
         </div>
         <div class="col-md-8 col-12">
             <div class="card-body overflow-hidden news-info" style="height:200px;">
-                <h5 class="card-title">${news.title}</h5>
-                <p class="card-text "><small class="text-muted">${news.details}</small></p>
+                <h5 class="card-title">${news.title ? news.title : 'title not fount'}</h5>
+                <p class="card-text "><small class="text-muted">${news.details ? news.details : 'details not found'}</small></p>
             </div>
             <div class="ms-3 d-flex align-items-center justify-content-between"> 
                 <div class="author-info d-flex align-items-center">
@@ -84,7 +92,7 @@ const displayAllNews = (allNews) => {
                 <div>
                 <i class="fa-solid fa-eye"></i> <span>${news.total_view ? news.total_view : 'view not found'}
                 </div>
-                <button class="btn btn-primary px-3" onclick="openModal('${news._id}')" data-bs-toggle="modal" data-bs-target="#exampleModal">Details</button>
+                <button class="btn btn-primary px-3" onclick="openModal('${news._id ? news._id : 'id not found'}')" data-bs-toggle="modal" data-bs-target="#exampleModal">Details</button>
             </div>
     </div>
         `
@@ -98,6 +106,7 @@ const openModal = (newsId) => {
     fetch(url)
         .then(res => res.json())
         .then(news => displayNewsDetails(news.data[0]))
+        .catch(error = console.log(error))
 
 }
 
@@ -108,7 +117,7 @@ const displayNewsDetails = (news) => {
     const modalBody = document.getElementById('modal-body');
     modalBody.innerHTML = `
     <div>
-        <img src="${news.image_url} " class="w-100"></img>
+        <img src="${news.image_url ? news.image_url : 'image not found'} " class="w-100"></img>
     </div>
     <p>${news.details ? news.details : 'no details found'}</p>
     <div class="d-flex author-info align-items-center justify-content-between">
@@ -145,7 +154,14 @@ const isLoading = (element) => {
 
 document.getElementById('NewsButton').addEventListener('click', function () {
     getAllNewsListData();
+    window.location.href = "index.html"
+
+
 })
+document.getElementById('blogButton').addEventListener('click', function () {
+    window.location.href = "blog.html"
+})
+
 
 getAllNewsListData();
 
